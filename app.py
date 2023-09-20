ba6b77354032c8b5d7768e6e512953d9512d22e1
Michael Rudy <michael4rudy@gmail.com>
2023-09-19 22:45:12 -0400

    updates

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-import json
-from rudycorp.utils import request
-from rudycorp import app, render_template
+from flask import Flask, render_template
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-	headlines = ['About', 'Resume', 'Interests', 'Projects', 'Contact']
+	info = {
+		'Name': 'Michael Rudy',
+		'headline': ['About', 'Education', 'Projects', 'Contact'],
+		'location': 'Washington, D.C.',
+		'email': 'michaelrudy4@icloud.com', 
+		'education': {'school': 'The George Washington University', 
+					  'degree': 'Bachelors in Information Systems; minor in Economics',
+					  'date': 'May 2021',
+					  'desc': """Notable coursework included Business Systems Development, 
+					  Database Application and Design, Python Programming, Financial Economics, Human Capital Economics, Intermediate Macroeconomic Theory, and Intermediate Microeconomic Theory."""},
+		
+	}
 	return render_template('home.html', info=info)
-
-# @app.route('/<string:page_name>')
-# def html_page(page_name):
-#     return render_template(page_name, info=info, works=works)
 	
 if __name__ == '__main__':
 	app.run(debug=True)
